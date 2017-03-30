dfdd1a4636150dcad0c29237efb468f933e61441
Aaron Crump <air.crump@gmail.com>
2017-03-29 23:26:36 -0400

    starting to work on BCDO code, it's not working

---
+++ cpu.ip_user_files/ip/cpuram/cpuram_stub.v
@@ -0,0 +1,24 @@
+// Copyright 1986-2016 Xilinx, Inc. All Rights Reserved.
+// --------------------------------------------------------------------------------
+// Tool Version: Vivado v.2016.3 (win64) Build 1682563 Mon Oct 10 19:07:27 MDT 2016
+// Date        : Thu Mar 16 18:38:22 2017
+// Host        : parikhc-PC running 64-bit Service Pack 1  (build 7601)
+// Command     : write_verilog -force -mode synth_stub
+//               c:/chirag/GVSUcourses/Winter17/EGR426/Projects/Project3_Mar23/cpu/cpu.srcs/sources_1/ip/cpuram/cpuram_stub.v
+// Design      : cpuram
+// Purpose     : Stub declaration of top-level module interface
+// Device      : xc7a35tcpg236-1
+// --------------------------------------------------------------------------------
+
+// This empty module with port declaration file causes synthesis tools to infer a black box for IP.
+// The synthesis directives are for Synopsys Synplify support to prevent IO buffer insertion.
+// Please paste the declaration into a Verilog source file or add the file as an additional source.
+(* x_core_info = "blk_mem_gen_v8_3_4,Vivado 2016.3" *)
+module cpuram(clka, wea, addra, dina, douta)
+/* synthesis syn_black_box black_box_pad_pin="clka,wea[0:0],addra[8:0],dina[7:0],douta[7:0]" */;
+  input clka;
+  input [0:0]wea;
+  input [8:0]addra;
+  input [7:0]dina;
+  output [7:0]douta;
+endmodule
